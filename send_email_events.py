bc4fc526343a4c2ff3a180c4d3197192b1723fe2
DavidSheinkman <Poilkjmnb!davidsheinkman7>
2026-02-21 23:01:29 +0100

    email link fix

--- send_email_events.py
+++ send_email_events.py
@@ -24,7 +24,7 @@ Start: {event['startTime']}
 Venue: {event['venue']['name']}
 
 Link:
-https://ra.co{event['contentUrl']}
+https://ra.co/events/{event['contentUrl']}
 """
 
     msg.set_content(body)
